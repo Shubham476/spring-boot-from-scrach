5012b4d41042941e14414fc34c1b639cd26a8298
shubham patil <shubhampatilraje476@gmail.com>
2026-07-02 23:53:41 +0530

    update code

---
+++ mvc/exception handling/src/main/java/com/example/springbootweb/advices/GlobalResponseHandler.java
@@ -0,0 +1,20 @@
+package com.example.springbootweb.advices;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+public class GlobalResponseHandler implements ResponseBodyAdvice {
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return false;
+    }
+
+    @Override
+    public @Nullable Object beforeBodyWrite(@Nullable Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        return null;
+    }
+}
