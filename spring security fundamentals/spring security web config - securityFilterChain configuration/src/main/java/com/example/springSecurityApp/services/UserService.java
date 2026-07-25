77724e20b3b28a5f1206cc77c0b1616aeaad268e
shubham patil <shubhampatilraje476@gmail.com>
2026-07-26 02:00:04 +0530

    spring security web config - securityFilterChain configuration

---
+++ spring security fundamentals/spring security web config - securityFilterChain configuration/src/main/java/com/example/springSecurityApp/services/UserService.java
@@ -0,0 +1,21 @@
+package com.example.springSecurityApp.services;
+
+import com.example.springSecurityApp.exception.ResourceNotFoundException;
+import com.example.springSecurityApp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+//@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(()-> new ResourceNotFoundException("user not found"));
+    }
+}
