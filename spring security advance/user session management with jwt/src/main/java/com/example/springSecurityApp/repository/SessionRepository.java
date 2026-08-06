e28b92aaa69e749bf001f95df0899e143e555b37
shubham patil <shubhampatilraje476@gmail.com>
2026-08-06 18:03:57 +0530

    user session management with jwt

---
+++ spring security advance/user session management with jwt/src/main/java/com/example/springSecurityApp/repository/SessionRepository.java
@@ -0,0 +1,13 @@
+package com.example.springSecurityApp.repository;
+
+import com.example.springSecurityApp.entities.Session;
+import com.example.springSecurityApp.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SessionRepository extends JpaRepository<Session, Long> {
+    List<Session> findByUser(User user);
+    Optional<Session> findByRefreshToken(String refreshToken);
+}
